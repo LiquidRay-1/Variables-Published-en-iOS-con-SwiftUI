ec673f2099e0882ed9e90f230c5810a62e325725
Iván Tortosa <152330066+LiquidRay-1@users.noreply.github.com>
2024-02-22 20:08:10 +0100

    Add files via upload

---
+++ Test2/Test2/ContentView.swift
@@ -0,0 +1,40 @@
+//
+//  ContentView.swift
+//  Test2
+//
+//  Created by dam2 on 22/2/24.
+//
+
+import SwiftUI
+
+class UserData: ObservableObject {
+    @Published var username: String = ""
+    // Una variable publicada notifica a la vista que la variabl ha cambiado de valor
+}
+
+struct ContentView: View {
+    
+    @StateObject var userData = UserData()
+    
+    var body: some View {
+        Form{
+            Text("¡Bienvenid@, \(userData.username)!")
+            UserProfileView()
+                .environmentObject(userData)
+        }
+        .padding()
+    }
+}
+
+struct UserProfileView: View{
+    
+    @EnvironmentObject var userData: UserData
+    
+    var body: some View{
+        TextField("Introduce tu nombre", text: $userData.username)
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Test2/Test2/Test2App.swift
@@ -0,0 +1,17 @@
+//
+//  Test2App.swift
+//  Test2
+//
+//  Created by dam2 on 22/2/24.
+//
+
+import SwiftUI
+
+@main
+struct Test2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
